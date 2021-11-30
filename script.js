a1d0b03153cc224badca9c3b122be7b8c2c3303d
deffitk <69427652+deffitk@users.noreply.github.com>
2021-11-30 12:11:17 -0500

    Join conditions in function checkdata

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function compute() {
     var years = document.getElementById("years").value;
     var interest = principal * years * rate / 100;
     var year = new Date().getFullYear() + parseInt(years);
-    var reult = document.getElementById("result").innerHTML = "If you deposit <mark>" + principal + "</mark>, <br/>an interest rate of <mark>" + rate + "% </mark>. <br/>You will receive an amount of <mark>" + interest + "</mark>, <br/>in the year <mark>" + year + "</mark><br/>"
+    var reult = document.getElementById("result").innerHTML = "If you deposit <mark>" + principal + "</mark>, <br/>at an interest rate of <mark>" + rate + "% </mark>. <br/>You will receive an amount of <mark>" + interest + "</mark>, <br/>in the year <mark>" + year + "</mark><br/>"
 }
 
 function updateRate() {
@@ -24,14 +24,8 @@ function checkdata() {
     //create reference to the input element 'principal'
     var principal = document.getElementById("principal");
 
-    //Check if principal field is empty
-    if (principal.value == "") {
-        alert("Please enter the Amount");
-        principal.focus();
-        return false;
-    }
-    //Check  if the user enters a negative number or cero
-    else if (principal.value <= 0) {
+    //Check if principal field is empty OR if the user enters a negative number or cero
+    if (principal.value <= 0 || principal.value == "") {
         alert("Please enter a positive number");
         principal.focus();
         return false;
